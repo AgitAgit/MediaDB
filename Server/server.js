e38ee253a2f1606a3b8afbbd7ec3cc6a3f37c9ba
Amit Kubani <amitkubani@gmail.com>
2024-08-27 14:00:03 +0300

    spotify api first successful GET

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,9 @@ const port = 3500;
 // });
 
 async function getToken(){
-    axios.post('https://accounts.spotify.com/api/token', {
+    let token = '';
+
+    await axios.post('https://accounts.spotify.com/api/token', {
         grant_type: 'client_credentials',
         client_id: "dfa2522400934fe6b7005ca8ddc3add9",
         client_secret: "d00c517b014443bdadb77e74b626415a",
@@ -31,17 +33,27 @@ async function getToken(){
         }
     })
     .then(response => {
-        console.log(response);
+        token = response.data.access_token;
     })
     .catch(error => {
     });
+
+    return token;
 }
-async function getData(){
-    axios.get('')
-    .then()
+async function getData(accessToken){
+    axios.get('https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb',{
+        headers:{
+            'Authorization': `Bearer ${accessToken}`
+        }
+    })
+    .then(data => {
+        console.log(data);
+    })
     .catch();
 }
 
 // connectDB();
-
-getToken();
+getToken()
+    .then(token => {
+        getData(token)
+    });
