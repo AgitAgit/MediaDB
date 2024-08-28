f2a5fffb747a7e7db5352a711b6d600b92dd46e6
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-08-28 18:51:21 +0300

    add

--- Server/booksHandler.js
+++ Server/booksHandler.js
@@ -2,18 +2,17 @@
 const axios = require('axios');
 
 // search query
-const query = 'JavaScript';
+const query = 'harry+potter';
 const baseUrl = `https://www.googleapis.com/books/v1/volumes`;
 const { googleBooksApi } = require('./config/apiKeys');
 
+const HarryPotterUrl = baseUrl + `?q=${'harry+potter'}&maxResults=${'5'}&key=${googleBooksApi}`;
+const williamShakespearURL = baseUrl + `?q=${'inauthor:william+shakespeare'}&maxResults=${'5'}&key=${googleBooksApi}`;
+const franchUrl = baseUrl + `?q=""&langRestrict=${'fr'}&maxResults=${'5'}&key=${googleBooksApi}`;
+
 function searchData() {
-    axios.get(baseUrl + `?q=${query}&${googleBooksApi}`)
-        .then(response => {
-            console.log('Books:', response.data.items[0]);
-            // response.data.items.forEach(book => {
-            //     console.log(book.volumeInfo.title);
-            // })
-        })
+    return axios.get(franchUrl)
+        .then(response => response.data.items)
         .catch(error => {
             console.error('Error fetching data:', error);
         });
