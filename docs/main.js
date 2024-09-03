9e375024f2e6d9ac5aaa419ca6498fdd4de667d2
agit <box999box@gmail.com>
2024-09-03 17:37:22 +0300

    commit

--- docs/main.js
+++ docs/main.js
@@ -5,7 +5,7 @@ const _dataPath = '/api/data';
 const _pokemonPath = '/api/pokemon';
 
 function testFunc(){
-    fetch(`${_localServerAdress}${_dataPath}`)
+    fetch(`${_cloudServerAdress}${_dataPath}`)
     .then(response => response.json())
     .then(data => {
         console.log(data);
@@ -16,7 +16,7 @@ function testFunc(){
 }
 
 function pokemonFetch(){
-    fetch(`${_localServerAdress}${_pokemonPath}`)
+    fetch(`${_cloudServerAdress}${_pokemonPath}`)
     .then(response => response.json())
     .then(data => {
         console.log(data);
