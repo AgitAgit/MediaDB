c1ac68752f324ede32b9b63992cd611ea0f5b294
agit <box999box@gmail.com>
2024-09-03 20:20:05 +0300

    commit

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const spHandle = require('./spotifyHandler');
 // const bkHandle = require('./booksHandler');
@@ -9,6 +10,17 @@ require('dotenv').config();
 
 const data = spHandle.searchData();
 data.then(data=>console.log(data));
+// const spotify_client_credentials = JSON.parse(process.env.spotify_client_credentials);
+// const data = axios.post('https://accounts.spotify.com/api/token', spotify_client_credentials,
+// {
+//     headers:{
+//         "Content-Type" : "application/x-www-form-urlencoded"
+//     }
+// });
+
+// data.then(response =>{
+//     console.log(response.data.access_token);
+// })
 app.use(cors());
 
 app.get('/api/ping', (req,res) =>{
--- Server/spotifyHandler.js
+++ Server/spotifyHandler.js
@@ -1,20 +1,15 @@
 require('dotenv').config();
 const axios = require('axios');
-const spotify_client_credentials = process.env.spotify_client_credentials;
+const spotify_client_credentials = JSON.parse(process.env.spotify_client_credentials);
 
 
 spHandle = {
-    token: '',
-
     getToken: function(){
         axios.post('https://accounts.spotify.com/api/token', spotify_client_credentials,
         {
             headers:{
                 "Content-Type" : "application/x-www-form-urlencoded"
             }
-        })
-        .then(response => {
-            this.token = response.data.access_token;
         })
         .catch(error => {
             console.log("error in  spotifyHandler:", error)
@@ -27,8 +22,7 @@ spHandle = {
     },
     
     searchData: async function(type="track", query="track%3Bat%2520country"){
-        if(this.token === '') this.getToken();
-        await this.token;
+                
         const data = await axios.get(`https://api.spotify.com/v1/search`,{
             params:{
                 q: query,
