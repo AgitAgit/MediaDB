0089c3a7e4121dfd1bb3397d7236021f3e50691a
Amit Kubani <amitkubani@gmail.com>
2024-09-04 10:05:13 +0300

    commit

--- Server/server.js
+++ Server/server.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-//const spHandle = require('./spotifyHandler');
+const spHandle = require('./spotifyHandler');
 // const bkHandle = require('./booksHandler');
+const { insertDB, findDB} = require('./cloudConn')
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 require('dotenv').config();
 
-const itemController = require('./controllers/itemController');
-const userController = require('./controllers/userController');
+
 app.use(cors());
 
 app.get('/api/ping', (req,res) =>{
     res.json({message:"Pong!"});
 })
 
-app.get('/api/data', itemController.getData);
-
 
 app.listen(port,()=>{
     console.log(`the server is listening on port ${port}`);
 });
 
-
-// bkHandle.connectDB();
