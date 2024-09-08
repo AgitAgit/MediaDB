4b5aa224ce620a2f152a060e6213838e75206005
agit <box999box@gmail.com>
2024-09-08 17:09:24 +0300

    commit

--- client/src/components/Songs/Song.jsx
+++ client/src/components/Songs/Song.jsx
@@ -1,7 +1,9 @@
-function Song({img, title='def title', album='def album', artist='def artist', songLink, albumLink, artistLink}){
+import defaultImg from "C:/Users/User/Learning/Software/projects/MediaDB/client/src/assets/DefaultAlbumCover.png";
+
+function Song({img=defaultImg, title='def title', album='def album', artist='def artist', songLink, albumLink, artistLink}){
     return(
         <div id="Song">
-            <img src={img} alt={title}></img>
+            <img src={img} alt={img}></img>
             <a href={songLink}><h1>{title}</h1></a>
             <a href={albumLink}><h3>album:{album}</h3></a>
             <a href={artistLink}><h3>artist:{artist}</h3></a>
--- client/src/components/Songs/Songs.jsx
+++ client/src/components/Songs/Songs.jsx
@@ -1,10 +1,22 @@
 import './Songs.css';
 import Song from './Song';
 
+//example data:
+const songData={_id:
+    '66d82f8dc2e4f6e9ba04ecf7',
+    link:
+    "https://open.spotify.com/track/2uuMXKKmdXrY2XxsqTsGp0",
+    spotifyId:
+    "2uuMXKKmdXrY2XxsqTsGp0",
+    track:
+    "Immigrant Song - Remaster",
+    album:
+    "Led Zeppelin"}
+
 function Songs(){
     //img, title, album, artist, songLink, albumLink, artistLink
     return(
-        <Song img='../../../public/DefaultAlbumCover.png' />
+        <Song title={songData.track} album={songData.album} artist={songData.artist} songLink={songData.link}/>
     );
 }
 
