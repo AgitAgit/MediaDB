60a800fbaad2eb064bce513cb023476c9399462f
agit <box999box@gmail.com>
2024-09-08 20:44:40 +0300

    commit

--- client/src/components/Songs/Song.jsx
+++ client/src/components/Songs/Song.jsx
@@ -1,10 +1,10 @@
-import defaultImg from "C:/Users/User/Learning/Software/projects/MediaDB/client/src/assets/DefaultAlbumCover.png";
 
-function Song({img=defaultImg, title='def title', album='def album', artist='def artist', songLink, albumLink, artistLink}){
+function Song(props){
+    const {img, track, album, artist, link, albumLink, artistLink} = props.data;
     return(
         <div id="Song">
             <img src={img} alt={img}></img>
-            <a href={songLink}><h1>{title}</h1></a>
+            <a href={link}><h1>{track}</h1></a>
             <a href={albumLink}><h3>album:{album}</h3></a>
             <a href={artistLink}><h3>artist:{artist}</h3></a>
         </div>
--- client/src/components/Songs/Songs.jsx
+++ client/src/components/Songs/Songs.jsx
@@ -1,8 +1,13 @@
 import './Songs.css';
 import Song from './Song';
+import defaultImg from "C:/Users/User/Learning/Software/projects/MediaDB/client/src/assets/DefaultAlbumCover.png";
+
 
 //example data:
-const songData={_id:
+const songData={
+    img:
+    defaultImg,
+    _id:
     '66d82f8dc2e4f6e9ba04ecf7',
     link:
     "https://open.spotify.com/track/2uuMXKKmdXrY2XxsqTsGp0",
@@ -13,10 +18,31 @@ const songData={_id:
     album:
     "Led Zeppelin"}
 
+    const songData2={
+        img:
+        defaultImg,
+        _id:
+        '66d82f8dc2e4f6e9ba04ecf7',
+        link:
+        "https://open.spotify.com/track/2uuMXKKmdXrY2XxsqTsGp0",
+        spotifyId:
+        "2uuMXKKmdXrY2XxsqTsGp0",
+        track:
+        "Other song",
+        album:
+        "Led Zeppelin"}
+
+    
 function Songs(){
     //img, title, album, artist, songLink, albumLink, artistLink
+    const songs=[songData,songData2,songData,songData2]
     return(
-        <Song title={songData.track} album={songData.album} artist={songData.artist} songLink={songData.link}/>
+        <div>
+            <Song data={songData}/>
+            {songs.map((song, index)=>{
+                return(<Song data={song} key={index}/>);
+            })}
+        </div>
     );
 }
 
