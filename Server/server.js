ebbf5d4941a848096f61a9a84c23d61326926c5e
agit <box999box@gmail.com>
2024-09-09 15:20:07 +0300

    commit

--- Server/server.js
+++ Server/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const {findOne} = require('./controllers/mongoActions');
-const spotifyHandler = require('./spotifyHandler');
-const bkHandle = require('./booksHandler');
+const spotifyHandler = require('./spotifyAPI');
+const bkHandle = require('./googleBooksAPI');
 const cors = require('cors');
 const port = process.env.PORT || 8080;
 require('dotenv').config();
 
-const itemController = require('./controllers/itemController');
+const bookHandler = require('./controllers/bookHandler');
+const songHandler = require('./controllers/songHandler');
+
 const userController = require('./controllers/userController');
 const mongoActions = require('./controllers/mongoActions');
 
@@ -18,8 +20,8 @@ app.get('/api/ping', (req,res) =>{
     res.json({message:"Pong!"});
 })
 
-app.get('/api/data', itemController.getData);
-
+app.get('/api/data/book/get', bookHandler.getData);
+app.get('/api/data/song/get' )
 app.listen(port,()=>{
     console.log(`the server is listening on port ${port}`);
 });
---
+++ client/src/dataCenter.js
@@ -0,0 +1,8 @@
+const _SERVER_ADDRESS = 'https://mediadb-91464205485.us-central1.run.app';
+const _SERVER_PORT = 8080;
+
+// function getBooks(){
+//     fetch(`${_SERVER_ADDRESS}:${_SERVER_PORT}/`)
+// }
+
+// function getSongs(){}
