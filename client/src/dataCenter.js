ff8c1cd641f965b8a3c7f8d9d353d9f536e884d2
agit <box999box@gmail.com>
2024-09-16 09:49:53 +0300

    commit

--- client/src/dataCenter.js
+++ client/src/dataCenter.js
@@ -5,7 +5,7 @@ const _SERVER_ADDRESS = 'https://mediadb-91464205485.us-central1.run.app';
 const _LOCAL_SERVER_ADDRESS = 'http://localhost';
 const _SERVER_PORT = 8080;
 
-const _CURRENT_ADDRESS = _LOCAL_SERVER_ADDRESS;
+const _CURRENT_ADDRESS = _SERVER_ADDRESS;
 
 
 function getBooks(searchText, method){
