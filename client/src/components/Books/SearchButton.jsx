95329c9ea76d168931cd20c01250b2aa7c5e48b4
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-09-18 11:09:48 +0300

    add

--- client/src/components/Books/SearchButton.jsx
+++ client/src/components/Books/SearchButton.jsx
@@ -2,16 +2,19 @@ import search_icon_black from './../../assets/search_icon-black.png';
 import search_icon_white from './../../assets/‏‏search_icon-white.png'
 
 function SearchButton(props){
-    const { searchText, setSearchText, theme } = props;
+    const { searchText, setSearchText, theme, setTriggerSearch } = props;
 
     function handleTextChange(event){
         setSearchText(event.target.value);
     }
+    function handleSearchClick(){
+        setTriggerSearch((prev) => !prev);
+    }
 
     return(
     <div id='search-container'>
-        <input type='text' value={searchText} onChange={handleTextChange} placeholder='Search for a book...' />
-        <img id='search-icon' src={theme === 'light' ? search_icon_black : search_icon_white} />
+        <input type='text' id='search-input-bar' value={searchText} onChange={handleTextChange} placeholder='Search for a book...' />
+        <img id='search-icon' onClick={handleSearchClick} src={theme === 'light' ? search_icon_black : search_icon_white} />
     </div>
     );
 }
