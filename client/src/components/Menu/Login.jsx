24f6d687afa83b37737915c23ae73403fc8178da
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-09-21 17:16:51 +0300

    add

--- client/src/components/Menu/Login.jsx
+++ client/src/components/Menu/Login.jsx
@@ -1,17 +1,58 @@
+import { useContext, useState, useRef } from "react";
+import { stateContext } from "./Menu";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+
 
 function Login() {
+    const { userLogged, setUserLogged } = useContext(stateContext);
+    const [EyeOpen, setEyeOpen] = useState(false);
+
+    const err = useRef(null);
+    const usernameRef = useRef(null);
+    const passwordRef = useRef(null);
     
-    const handleSubmit = async (e) => {
+    const handleSubmitLogin = async (e) => {
         e.preventDefault();
+        const user = 'yaniv'; //Check data base
+        if(user)
+            setUserLogged(user);
+        else {
+            err.current.style.display = "block";
+            for(const input of [usernameRef,passwordRef]){
+                input.current.style.borderColor = "red";
+                input.current.classList.add('shake');
+                setTimeout(() => {
+                    input.current.classList.remove('shake');
+                }, 500);
+            }
+        }
+    }
+    function handleGuestClick() {
+        setUserLogged('guest');
+    }
+    const handleInputChange = (e) => {
+        usernameRef.current.style.borderColor = passwordRef.current.style.borderColor = "rgb(193, 193, 193)";
+        err.current.style.display = "none";
     }
 
+    function toggleEye() {
+        setEyeOpen(prev => !prev)
+    }
     return (
-        <form id='login-form' className="menu-form" onSubmit={handleSubmit}>
+        <form id='login-form' className="menu-form" onSubmit={handleSubmitLogin}>
             <div>
-                <input id="username-login" className="menu-input" placeholder="Username" maxLength={15}/>
-                <input id="password-login" className="menu-input" placeholder="Password" maxLength={15}/>
+                <input id="username-login" className="menu-input" ref={usernameRef} onChange={handleInputChange} placeholder="Username" maxLength={15}/>
+                <div className='position-relative'>
+                    <input type={EyeOpen ? "text": "password"} id="password-login" className="menu-input"
+                    ref={passwordRef} onChange={handleInputChange} placeholder="Password" maxLength={15}/>
+                    {EyeOpen ? <FontAwesomeIcon icon={faEye} onClick={toggleEye} className="open-eye eye"/> :
+                    <FontAwesomeIcon icon={faEyeSlash} onClick={toggleEye} className="close-eye eye"/>}
+                </div>
+                <p className="error login-invalid" ref={err}>Invalid username or password</p>
                 <p id="forgot-password" className="link">Forgot Password?</p>
-                <p>Commitment-phobe?  <span className="link">Go ahead as a guest!</span></p>
+                <p>Commitment-phobe?  <span className="link" onClick={handleGuestClick}>Go ahead as a guest!</span></p>
             </div>
             <button id="submit-login" className="submit-button" type="submit">Login</button>
         </form>
--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -9,32 +9,29 @@ import Header from '../Books/Header.jsx';
 import Footer from '../Books/Footer.jsx';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
+import SelectionMenu from './SelectionMenu.jsx';
 
 export const stateContext = createContext();
 
 function Menu(){
     const [state, setState] = useState('Menu');
-    const [isLoginActive, setIsLoginActive] = useState(true);
     const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const [loginForm, setLoginForm] = useState(true);
+    const [userLogged, setUserLogged] = useState(null);
 
     document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
-
-    // useEffect(() => {
-    //     document.documentElement.setAttribute('data-theme', theme);
-    //     localStorage.setItem('theme', theme);
-    // }, [theme]);
 
     function toggleTheme(){
         setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
     }
 
     const handleLoginClick = () => {
-        setIsLoginActive(true);
+        setLoginForm(true);
     };
 
     const handleSignupClick = () => {
-        setIsLoginActive(false);
+        setLoginForm(false);
     };
     function onBooksClick(){
         setState('Books');
@@ -44,42 +41,44 @@ function Menu(){
     }
 
     return(
-            <stateContext.Provider value={{state, setState, theme, setTheme}}>
-                {state === 'Books' && (
-                    <Books/>
-                )}
-                {state === 'Songs' && (
-                    <Songs/>
-                )}
-                {state === 'Menu' &&(
-                    <div id="Menu">
-                        <div id='main-content-menu'>
-                            <button className='temp1' onClick={onBooksClick}>Books</button>
-                            <button className='temp2' onClick={onSongsClick}>Songs</button>
-                            <div className='background'></div>
-                            <div className="form-container">
-                                <div id="menu-theme" onClick={toggleTheme}>
-                                {theme === 'light' ? '☀️' : '🌙'}</div>
-                                <div className="button-box">
-                                    <div id="btn"
-                                    style={{left: isLoginActive ? "0px" : "110px",}}
-                                    ></div>
-                                    <button className="toggle-btn" onClick={handleLoginClick}>Log in</button>
-                                    <button className="toggle-btn" onClick={handleSignupClick}>Sign up</button>
-                                </div>
-                                    {isLoginActive ? <Login /> : <Signup />}
-                                    
+        <stateContext.Provider value={{state, setState, theme, setTheme , userLogged, setUserLogged}}>
+            {state === 'Books' && (
+                <Books/>
+            )}
+            {state === 'Songs' && (
+                <Songs/>
+            )}
+            {state === 'Menu' &&(
+                <div id="Menu">
+                    <div id='main-content-menu'>
+                        <button className='temp1' onClick={onBooksClick}>Books</button>
+                        <button className='temp2' onClick={onSongsClick}>Songs</button>
+                        <div className='background'></div>
+                        { !userLogged ? 
+                        //NO USER LOGGED
+                        <div className="form-container">
+                            <div id="menu-theme" onClick={toggleTheme}>
+                            {theme === 'light' ? '☀️' : '🌙'}</div>
+                            <div className="button-box">
+                                <div id="btn"
+                                style={{left: loginForm ? "0px" : "110px",}}
+                                ></div>
+                                <button className="toggle-btn" onClick={handleLoginClick}>Log in</button>
+                                <button className="toggle-btn" onClick={handleSignupClick}>Sign up</button>
                             </div>
-                            <div className='logo-container'>
-                                <img id='logo-large-menu' src={logoLarge} />
-                                {/* <h1>Find Your Art</h1> */}
-                                <h1>Your Playlist<br/> of Stories</h1>
-                            </div>
-
+                                {loginForm ? <Login /> : <Signup />}
+                        </div>
+                        : // USER LOGGED
+                        <SelectionMenu />
+                        }
+                        <div className='logo-container'>
+                            <img id='logo-large-menu' src={logoLarge} />
+                            <h1>Your Playlist<br/> of Stories</h1>
                         </div>
                     </div>
-                )}
-            </stateContext.Provider>
+                </div>
+            )}
+        </stateContext.Provider>
     )
 }
 
