7e8c8efe50978d2d5bbefc9955dfd238f360136f
Amit Kubani <amitkubani@gmail.com>
2024-09-24 12:05:10 +0300

    commit

--- client/src/components/Songs/SearchBar.jsx
+++ client/src/components/Songs/SearchBar.jsx
@@ -3,7 +3,11 @@ import {searchContext} from './Songs';
 import searchIcon from './../../assets/search1.svg';
 function SearchBar(){
     const onSearchClick = useContext(searchContext);
-
+    function handleFavoritesClick(event){
+        event.target.classList.toggle('btnClicked');
+        //now I need to add to a context the favorites btn state,
+        //and display songs based on that.
+    }
     return(
         <div class="searchBar">
             <div class="searchField">
@@ -11,7 +15,9 @@ function SearchBar(){
                 <input id="searchInput" onChange={() => onSearchClick(document.getElementById('searchInput').value, document.getElementById('searchSelect').value)}>
                 </input>
             </div>
-            
+            <div class="favoritesDiv">
+                <button class="favoritesBtn" onClick={handleFavoritesClick}>Favorites</button>
+            </div>
             <div class="selectDiv">
                 <span>search by:</span>
                 <select id="searchSelect" placeholder='Track' onChange={() => onSearchClick(document.getElementById('searchInput').value, document.getElementById('searchSelect').value)}>
