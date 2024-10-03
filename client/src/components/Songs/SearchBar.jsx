1e9df43747089ef95ffe7509f91ba308e2d959e3
agit <box999box@gmail.com>
2024-10-03 13:33:42 +0300

    added features to search bar

--- client/src/components/Songs/SearchBar.jsx
+++ client/src/components/Songs/SearchBar.jsx
@@ -5,6 +5,62 @@ import searchIcon from './../../assets/search1.svg';
 function SearchBar(){
     const { onSearchClick } = useContext(searchContext);
     const [favBtnOn, setFavBtnOn] = useState(false);
+    const artists = [
+        "AC/DC",
+        "Adele",
+        "Aerosmith",
+        "Ariana Grande",
+        "Aretha Franklin",
+        "Bad Bunny",
+        "Beyoncé",
+        "Billie Eilish",
+        "The Black Eyed Peas",
+        "Bob Dylan",
+        "Bruce Springsteen",
+        "Bruno Mars",
+        "BTS",
+        "The Chainsmokers",
+        "Coldplay",
+        "David Bowie",
+        "Doja Cat",
+        "Drake",
+        "Dua Lipa",
+        "Ed Sheeran",
+        "Elton John",
+        "Eminem",
+        "Elvis Presley",
+        "George Harrison",
+        "Guns N' Roses",
+        "Harry Styles",
+        "Imagine Dragons",
+        "Jennifer Lopez",
+        "John Lennon",
+        "Justin Bieber",
+        "Kanye West",
+        "Lady Gaga",
+        "Led Zeppelin",
+        "Maroon 5",
+        "Michael Jackson",
+        "Metallica",
+        "Olivia Rodrigo",
+        "Paul McCartney",
+        "Pink",
+        "Pink Floyd",
+        "Post Malone",
+        "Prince",
+        "Queen",
+        "Rihanna",
+        "Ringo Starr",
+        "Rolling Stones, The",
+        "Shakira",
+        "Taylor Swift",
+        "The Beatles",
+        "The Weeknd",
+        "Whitney Houston"
+      ];
+    const artistSelectRef = useRef(null);
+    const artistSelectDivRef = useRef(null);
+
     function handleFavoritesClick(event){
         const fav = !favBtnOn;
         event.target.classList.toggle('btnClicked');
@@ -12,6 +68,31 @@ function SearchBar(){
         onSearchClick(document.getElementById('searchInput').value, document.getElementById('searchSelect').value, fav);
         
     }
+
+    function handleSelectChange(event){
+        if(event.target.value === 'Artist'){
+            artistSelectDivRef.current.classList.remove('hidden');
+        }
+        else{
+            artistSelectDivRef.current.classList.add('hidden');
+        }
+    }
+
+    function initArtistSelect(){
+        while(artistSelectRef.current.firstChild){
+            artistSelectRef.current.removeChild(artistSelectRef.current.firstChild);
+        }
+        for(let i = 0; i < artists.length; i++){
+            const option = document.createElement('option');
+            option.textContent = artists[i];
+            artistSelectRef.current.appendChild(option);
+        }
+    }
+
+    useEffect(() => {
+        initArtistSelect();
+    });
+
     return(
         <div className="searchBar">
             <div className="searchField">
@@ -19,20 +100,20 @@ function SearchBar(){
                 <input id="searchInput" onChange={() => onSearchClick(document.getElementById('searchInput').value, document.getElementById('searchSelect').value)}>
                 </input>
             </div>
-            {/* <div className="selectDiv">
+            <div ref={artistSelectDivRef} className="artistSelectDiv hidden">
                 <span>Artists in db:</span>
-                <select id="artistSelect" placeholder='Artist' onChange={() => onSearchClick(document.getElementById('searchInput').value, document.getElementById('searchSelect').value)}>
-                    <option>Track</option>
-                    <option>Album</option>
-                    <option>Artist</option>
+                <select ref={artistSelectRef} class="artistSelect" defaultValue='Artist' onChange={() => onSearchClick(artistSelectRef.current.value, document.getElementById('searchSelect').value)}>
                 </select>
-            </div> */}
+            </div>
             <div className="favoritesDiv">
                 <button className="favoritesBtn" onClick={handleFavoritesClick}>Favorites</button>
             </div>
             <div className="selectDiv">
                 <span>search by:</span>
-                <select id="searchSelect" placeholder='Track' onChange={() => onSearchClick(document.getElementById('searchInput').value, document.getElementById('searchSelect').value)}>
+                <select id="searchSelect" placeholder='Track' onChange={(event) => {
+                    onSearchClick(document.getElementById('searchInput').value, document.getElementById('searchSelect').value)
+                    handleSelectChange(event);
+                    }}>
                     <option>Track</option>
                     <option>Album</option>
                     <option>Artist</option>
