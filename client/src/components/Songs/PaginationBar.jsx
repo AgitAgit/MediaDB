eb4979579f075d8f9ff4a85e92a067da24bfad80
Amit Kubani <amitkubani@gmail.com>
2024-10-05 15:56:20 +0300

    commit

--- client/src/components/Songs/PaginationBar.jsx
+++ client/src/components/Songs/PaginationBar.jsx
@@ -7,12 +7,27 @@ let leftMiddle = 2;
 let middle = 3;
 let rightMiddle = 4;
 
+
+//I want the button with the current page to be marked.
+//There are two methods to choose the page. The arrows and clicking.
+//Ideally I would like the buttons to be iterable, and to add and remove the marker according to the current page value.
+//If I rely on the numerical value and not the placement it would cover both the arrows and clicking navigation.
+
+//So I'll get a ref to the pagination bar, create a list of all it's child nodes who are buttons. Remove the marked class from all and add
+//it to the one with the fitting numerical value.
+
 function PaginationBar(){
     const [currentPage, setCurrentPage] = useState(1);
+    const pBar = useRef(null);
     const leftSpanRef = useRef(null);
     const rightSpanRef = useRef(null);
     console.log('current page:', currentPage);
-    
+    // mark(currentPage);
+    //When I click on the middle left or middle right buttons, it marks the button I clicked and not the one
+    //with the correct value. Meaning it refers to the button that had the value at the time of clicking, and
+    //not the one the turns out to have the value after the re-render. So I need somehow to trigger "mark()" 
+    //after the page has been re-rendered. maybe trigger it on the onchange of pBar and have it use the current page as value.
+
     // useEffect(() => {
     // },[currentPage, leftMiddle, middle, rightMiddle]);
     
@@ -29,10 +44,22 @@ function PaginationBar(){
         else {
             leftSpanRef.current.textContent = '';
         }
-        
+        mark();
     },[currentPage, leftMiddle, middle, rightMiddle]);
 
+    function mark(page = currentPage){
+        const buttons = pBar.current.querySelectorAll('button');
+        buttons.forEach(button => {
+            button.classList.remove('marked');
+            if(parseInt(button.textContent) && parseInt(button.textContent) === page){
+                button.classList.add('marked');
+                console.log("page to be marked:",page);
+            }
+        })
+    }
+
     function handleNavTo(page){
+        if(page > TOTAL_PAGES || page < 1) return;
         setCurrentPage(page);
         console.log('nav to ', page);
         if(TOTAL_PAGES >= 5)
@@ -77,7 +104,7 @@ function PaginationBar(){
     }
     console.log("left:",leftMiddle," middle:",middle," right:",rightMiddle);
     return(
-        <div className="SongsPaginationBar">
+        <div ref={pBar} className="SongsPaginationBar">
             <button onClick={() => handleNavTo(currentPage - 1)}>{'<'}</button>
             <button onClick={handleNavToText}>{'1'}</button>
             <span ref={leftSpanRef}></span>
